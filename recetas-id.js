273bdbc8734ff43af70ffbbf006955a2d6d158ba
Nuria <nuria.01@hotmail.es>
2022-06-09 19:54:15 +0100

    recetas por id

--- recetas-id.js
+++ recetas-id.js
@@ -3,53 +3,92 @@ requestRecipe();
 async function requestRecipe() {
   try {
     const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get("id");
+    const id = urlParams.get("idreceta");
+    console.log(id);
     const response = await fetch(`${url}/recetas/` + id + `?populate=*`);
+
     if (!response.ok) {
       const message = `Error: ${response.status}`;
       throw new Error(message);
     }
     const recipeResponse = await response.json();
-    showRecipe(recipeResponse.recetas);
+    console.log(recipeResponse);
+
+
+
+    showRecipesMoreInfo(recipeResponse.data);
   } catch (error) {
     console.log(error);
   }
 }
 
-function showRecipe(recetas) {
-  const recipe = document.getElementById("receta-id");
-  recipe.innerHTML = "";
-  for (const receta of recetas) {
-    const div = document.createElement("div");
-    const titulo = document.createElement("h2");
-    const foto = document.createElement("img");
-    const ingredientes = document.createElement("ul");
-    const descripcion = document.createElement("p");
 
-    div.classList.add("contenedor");
-    foto.classList.add("imagen-receta");
 
-    foto.src = receta.data.attributes.foto;
-    titulo.textContent = receta.data.attributes.titulo;
+function showRecipesMoreInfo(data) {
 
-    for (var ingrediente of receta.data.attributes.ingredientes.data) {
-      const ingredienteItem = document.createElement("li");
+  console.log("chahco loco mira ver entra aqui");
+  console.log(data);
 
-      ingredienteItem.innerText += ingrediente.attributes.cantidad;
-      ingredienteItem.innerText += "  ";
-      ingredienteItem.innerText += ingrediente.attributes.Unidades + " de";
-      ingredienteItem.innerText += "  ";
-      ingredienteItem.innerText += ingrediente.attributes.nombre;
-      ingredientes.appendChild(ingredienteItem);
-    }
 
-    descripcion.textContent = receta.data.attributes.descripcion;
-    descripcion.innerHTML = descripcion.innerHTML.replaceAll("\n", "<br/>");
+  const listRecipes = document.getElementById("lista-recetas");
+  listRecipes.innerHTML = "MAITEEEEEEE";
 
-    div.appendChild(foto);
-    div.appendChild(titulo);
-    div.appendChild(ingredientes);
-    div.appendChild(descripcion);
-    recipe.appendChild(div);
+
+  receta = data;
+
+
+
+  // for (const receta of data) {
+
+  console.log("--------------------------------------------");
+  console.log(data[receta]);
+
+
+  const div = document.createElement("div");
+  const titulo = document.createElement("h2");
+  const foto = document.createElement("img");
+  const ingredientes = document.createElement("ul");
+  const descripcion = document.createElement("p");
+
+  div.classList.add("contenedor");
+  foto.classList.add("imagen-receta");
+
+  foto.src = receta.attributes.foto;
+  titulo.textContent = receta.attributes.titulo;
+
+  for (var ingrediente of receta.attributes.ingredientes.data) {
+    const ingredienteItem = document.createElement("li");
+
+    ingredienteItem.innerText += ingrediente.attributes.cantidad;
+    ingredienteItem.innerText += "  ";
+    ingredienteItem.innerText += ingrediente.attributes.Unidades + " de";
+    ingredienteItem.innerText += "  ";
+    ingredienteItem.innerText += ingrediente.attributes.nombre;
+    ingredientes.appendChild(ingredienteItem);
   }
+
+  descripcion.textContent = receta.attributes.descripcion;
+  descripcion.innerHTML = descripcion.innerHTML.replaceAll("\n", "<br/>");
+
+  div.appendChild(foto);
+  div.appendChild(titulo);
+  div.appendChild(ingredientes);
+  div.appendChild(descripcion);
+  listRecipes.appendChild(div);
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
--- recetas.js
+++ recetas.js
@@ -23,6 +23,7 @@ async function requestRecipes() {
 }
 
 function showRecipes(data) {
+  console.log(data);
   const listRecipes = document.getElementById("lista-recetas");
   listRecipes.innerHTML = "";
   for (const receta of data) {
@@ -33,8 +34,8 @@ function showRecipes(data) {
 
     div.classList.add("contenedor");
     foto.classList.add("imagen-receta");
-
-    a.href = "recetas-id.html/" + receta.id + "?populate=*";
+    //https://proyecto-final-mayte-nuria.herokuapp.com/api
+    a.href = "recetas-id.html?idreceta=" + receta.id;
     foto.src = receta.attributes.foto;
     titulo.textContent = receta.attributes.titulo;
 
@@ -42,5 +43,44 @@ function showRecipes(data) {
     div.appendChild(titulo);
     a.appendChild(div);
     listRecipes.appendChild(a);
+  }
+}
+
+
+function showRecipesMoreInfo(data) {
+  const listRecipes = document.getElementById("lista-recetas");
+  listRecipes.innerHTML = "";
+  for (const receta of data) {
+    const div = document.createElement("div");
+    const titulo = document.createElement("h2");
+    const foto = document.createElement("img");
+    const ingredientes = document.createElement("ul");
+    const descripcion = document.createElement("p");
+
+    div.classList.add("contenedor");
+    foto.classList.add("imagen-receta");
+
+    foto.src = receta.attributes.foto;
+    titulo.textContent = receta.attributes.titulo;
+
+    for (var ingrediente of receta.attributes.ingredientes.data) {
+      const ingredienteItem = document.createElement("li");
+
+      ingredienteItem.innerText += ingrediente.attributes.cantidad;
+      ingredienteItem.innerText += "  ";
+      ingredienteItem.innerText += ingrediente.attributes.Unidades + " de";
+      ingredienteItem.innerText += "  ";
+      ingredienteItem.innerText += ingrediente.attributes.nombre;
+      ingredientes.appendChild(ingredienteItem);
+    }
+
+    descripcion.textContent = receta.attributes.descripcion;
+    descripcion.innerHTML = descripcion.innerHTML.replaceAll("\n", "<br/>");
+
+    div.appendChild(foto);
+    div.appendChild(titulo);
+    div.appendChild(ingredientes);
+    div.appendChild(descripcion);
+    listRecipes.appendChild(div);
   }
 }
